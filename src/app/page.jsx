883c29050ef71e1eae486457bb2ffa21439f32b2
salazarxs <martinpaolo10@gmail.com>
2024-01-23 23:33:28 -0300

    cta products

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react";
 import styles from "./page.module.css";
 import HorizontalCard from "@/components/HorizontalCard";
 import { GetProducts } from "./helpers/GetProducts";
+import Link from "next/link";
 
 export default function Home() {
   const [products, setProducts] = useState();
@@ -26,19 +27,30 @@ export default function Home() {
         <p>Order it for you or for your beloved ones</p>
         <div className={styles.products}>
           {products && products.length ?
-            products.map(product => (
-              <HorizontalCard
-                key={product.id}
-                productID={product.id}
-                productName={product.productName}
-                img={product.productImage}
-                price={product.price}
-              />
-            ))
+            <>
+              {
+                products.map(product => (
+                  <HorizontalCard
+                    key={product.id}
+                    productID={product.id}
+                    productName={product.productName}
+                    img={product.productImage}
+                    price={product.price}
+                  />
+
+
+                ))
+              }
+              <Link
+                href={'/products'}
+                className={styles.seeMoreProducts}
+              >See more</Link>
+            </>
             : 'Loading products...'
           }
+
         </div>
       </div>
-    </main>
+    </main >
   );
 }
